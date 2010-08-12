55bc80f9deb87dd7ab18cc32a69247c9fcc71b01
trapd00r <trapd00r@trapd00r.se>
2010-08-12 08:31:29 +0200

    matrix.c: added full unicode support, restore colors on termination (for evil shells like bash)

--- matrix.c
+++ matrix.c
@@ -18,37 +18,39 @@ void hide_cursor(void) {
 }
 
 void show_cursor(void) {
- printf("\033[?25h");
+ printf("\033[?25h\033[0m\n");
 }
 
 void set_matrix_font(void) {
-  printf("\033]710;5x8\007\033]711;6x13\007");
+  printf("\033]710;5x7\007\033]711;6x13\007");
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, wchar_t *argv[]) {
   int actual_color, charslen;
-  char *charptr = NULL;
+  wchar_t *charptr = NULL;
   int green[GREEN_MAX] = {22, 28, 34, 40, 41, 34, 35};
-  char tokens[] = "01";
+  wchar_t tokens[] = L"\vabcdefABCDEF0123456789;«¸­³½§Ì¬±ÊÏ¶¿ÃÌÅ¦;(){}[]";
   char *tab_or_not = NULL;
-  wchar_t paraply[] = L"Ϩ";
 
   /* calls show_cursor() at program exit, even if terminated by a SIGINT */
   atexit(show_cursor);
   setlocale(LC_ALL,"");
 
   charptr = (argc > 1) ? argv[1] : tokens;
-  charslen = strlen(charptr);
+  charslen = wcslen(charptr);
 
   set_matrix_font();
   hide_cursor();
   while(1) {
     actual_color = green[rand() % GREEN_MAX];
-    tab_or_not = (actual_color % 80 == 0) ? "\033[3m\n" : "\t\033[1m";
+    tab_or_not   = (actual_color % 80 == 0) ? "\n" : "\t";
 
-    printf("%s\033[38;5;%dm%c\033[0m\033[38;5;%dm%ls",
-        tab_or_not, actual_color, *(charptr + (rand() % charslen)),
-        actual_color+2,paraply
+    printf(
+        "%s \033[38;5;%dm%c\033[38;5;%dm%c\033[38;5;%dm%c",
+        tab_or_not,
+        green[rand() % GREEN_MAX], *(charptr + (rand() % charslen)),
+        green[rand() % GREEN_MAX], *(charptr + (rand() % charslen)),
+        green[rand() % GREEN_MAX], *(charptr + (rand() % charslen))
         );
   }
   return(0);
