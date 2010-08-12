130a1756b82f4414ea70b0d4fcb872752613374b
trapd00r <trapd00r@trapd00r.se>
2010-08-12 07:07:03 +0200

    matrix.c: replaced tabs with spaces, keep stuff at <= 80 columns

--- matrix.c
+++ matrix.c
@@ -14,43 +14,43 @@
 #define UNICODE
 
 void hide_cursor(void) {
-	printf("\033[?25l");
+  printf("\033[?25l");
 }
 
 void show_cursor(void) {
-	printf("\033[?25h");
+ printf("\033[?25h");
 }
 
 void set_matrix_font(void) {
-	printf("\033]710;5x8\007\033]711;6x13\007");
+  printf("\033]710;5x8\007\033]711;6x13\007");
 }
 
 int main(int argc, char *argv[]) {
-	int actual_color, charslen;
-	char *charptr = NULL;
-	int green[GREEN_MAX] = {22, 28, 34, 40, 41, 34, 35};
-	char tokens[] = "01";
-	char *tab_or_not = NULL;
-	wchar_t paraply[] = L"Ϩ";
+  int actual_color, charslen;
+  char *charptr = NULL;
+  int green[GREEN_MAX] = {22, 28, 34, 40, 41, 34, 35};
+  char tokens[] = "01";
+  char *tab_or_not = NULL;
+  wchar_t paraply[] = L"Ϩ";
 
-	atexit(show_cursor); /* calls show_cursor() at program exit, even if terminated with ctrl+c */
-	setlocale(LC_ALL,"");
+  /* calls show_cursor() at program exit, even if terminated by a SIGINT */
+  atexit(show_cursor);
+  setlocale(LC_ALL,"");
 
-	charptr = (argc > 1) ? argv[1] : tokens;
-	charslen = strlen(charptr);
+  charptr = (argc > 1) ? argv[1] : tokens;
+  charslen = strlen(charptr);
 
-	set_matrix_font();
-	hide_cursor();
-	while(1) {
-		actual_color = green[rand() % GREEN_MAX];
-		tab_or_not = (actual_color % 40 == 0) ? "\n\033[3m" : "\t\033[1m";
+  set_matrix_font();
+  hide_cursor();
+  while(1) {
+    actual_color = green[rand() % GREEN_MAX];
+    tab_or_not = (actual_color % 80 == 0) ? "\033[3m\n" : "\t\033[1m";
 
-		printf("%s\033[38;5;%dm%c\033[0m\033[38;5;%dm%ls",
-		tab_or_not, actual_color, *(charptr + (rand() % charslen)),actual_color+2,paraply
-	);
-
-	usleep(800);
-	}
-	return(0);
+    printf("%s\033[38;5;%dm%c\033[0m\033[38;5;%dm%ls",
+        tab_or_not, actual_color, *(charptr + (rand() % charslen)),
+        actual_color+2,paraply
+        );
+  }
+  return(0);
 }
 
